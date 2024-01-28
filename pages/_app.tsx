3513dc8f00d011da82592b114622da4c3423d44d
TheDevWeb3 <157993024+TheDevWeb3@users.noreply.github.com>
2024-01-28 15:41:28 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,12 @@
 import type { AppProps } from "next/app";
-import {
-  ThirdwebProvider,
-  embeddedWallet,
-  smartWallet,
-} from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, useContract, useTokenBalance, walletConnect, } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import Navbar from "../components/navbar";
-import Header from "../components/header";
+
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = "polygon"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,17 +14,25 @@ function MyApp({ Component, pageProps }: AppProps) {
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
       supportedWallets={[
-        smartWallet(embeddedWallet(), {
-          factoryAddress: "0x9838b534cd5950CB6ea9E7fa94c00CF3986F953B",
-          gasless: true,
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        embeddedWallet({
+          auth: {
+            options: [
+              "email",
+              "google",
+              "apple",
+              "facebook",
+            ],
+          },
         }),
       ]}
     >
-      <Header />
       <Component {...pageProps} />
-      <Navbar />
     </ThirdwebProvider>
   );
 }
+
 
 export default MyApp;
